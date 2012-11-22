a77ea4142b777fd223a97b159c752630ecadd638
Roisatul Azizah <roisa.mscdr@gmail.com>
2012-11-22 15:29:41 +0700

    Update assets/js/init.js

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,17 +1,56 @@
 var init = {
     'questions': 
 	[{
-
+        'question': '1. Pengertian besaran adalah...',
+        'answers': ['a. sesuatu yang besar', 'b. sesuatu yang dapat dilihat', 'c. sesuatu yang dapat diukur', 'd. sesuatu yang dapat dilihat dan di ukur','e. sesuatu yang dapat diukur yang dinyatakan dengan angka'],
+        'correctAnswer': 5
+    }, {
+        'question': '2. Kelompok besaran pokok diantaranya...',
+        'answers': ['a. meter - kilogram - detik', 'b. panjang - massa - waktu', 'c. gram - cm - menit', 'd. volume - isi - luas','e. detik - panjang - massa'],
+        'correctAnswer': 2    
+    }, {
+        'question': '3. Pengertian zat adalah...',
+        'answers': ['a. sesuatu yang bisa diraba dan dirasakan', 'b. sesuatu yang menempati ruang dan memiliki massa', 'c. sesuatu yang memiliki energi dan massa', 'd. sesuatu yang memiliki bentuk dan massa','e. sesuatu yang bisa diraba dan memiliki energi'],
+        'correctAnswer': 2
+	}, {
+		'question': '4. Massa jenis yang terbesar dari zat berikut ini adalah...',
+        'answers': ['a. alumunium', 'b. besi ', 'c. emas', 'd. platina','e. alkohol'],
+        'correctAnswer': 4
+    
+	}, {
+		'question': '5. Bentuk lintasan planet adalah...',
+        'answers': ['a. bulat', 'b. streamline', 'c. elips', 'd. bulat dan streamline','e. elips dan streamline'],
+        'correctAnswer': 3
+    }, {
+		'question': '6. Waktu yang diperlukan planet untuk berputar satu kali disebut...',
+        'answers': ['a. orbit', 'b. ekliptika', 'c. kala revolusi', 'd. kala rotasi','e. kala ekliptika'],
+        'correctAnswer': 4
+	}, {
+		'question': '7. Benda dikatakan bergerak jika...',
+        'answers': ['a. mengalami perpindahan', 'b. mengalami proses', 'c. mengalami perubahan bentuk', 'd. mengalami perubahan suhu','e. mengalami perubahan susunan benda ini'],
+        'correctAnswer': 1
+	}, {
+		'question': '8. Pertambahan kecepatan dalam selang waktu tertentu disebut...',
+        'answers': ['a. berkecepatan', 'b. percepatan', 'c. penambahan', 'd. perubahan','e. perubahan kecepatan'],
+        'correctAnswer': 2
+	}, {
+		'question': '9. Sebuah benda dilempar ke atas, akhirnya jatuh kebawah. Peristiwa itu terjadi karena adanya...',
+        'answers': ['a. gaya tarik', 'b. gaya otot', 'c. gaya jatuh', 'd. gaya gesek','e. gaya gravitasi bumi'],
+        'correctAnswer': 5
+	}, {
+		'question': '10. Satuan gaya menurut SI adalah...',
+        'answers': ['a. newton', 'b. joule', 'c. hertz', 'd. coulomb', 'e. ohm'],
+        'correctAnswer': 1
 	}, 
 	],
     'resultComments': {
-        perfect: '',
-        excellent: '',
-        good: '',
-        average: '',
-        bad: '',
-        poor: '',
-        worst: ''
+        perfect: 'Sempurna, Pertahankan Prestasimu',
+        excellent: 'Memuaskan, Tigkatkan Lagi',
+        good: 'Bagus, Lebih Giat Belajar Lagi',
+        average: 'Sedang, Lebih Giat Belajar Lagi ',
+        bad: 'Kurang, Lebih Giat Belajar Lagi',
+        poor: 'Kurang, Rajin-rajinlah Belajar',
+        worst: 'Sangat Kurang, Rajin-rajinlah Belajar'
     }
 
 };
